6b7369a796bcd33b18fa185abc90e20dee4bb92f
Charles Michael Craven <charlesmcraven@students.abtech.edu>
2019-03-04 12:33:25 -0500

    updated index.php

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
 <!DOCTYPE html>
+<html>
 
-<html lang="en">
-
-  
 <head>
-  <title>Asgn 08 WEB 182</title>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Asgn03</title>
+  <link href="../css/style.css" rel="stylesheet">
 </head>
+
 <body>
-<h1>asgn08</h1>
-<?php
+  <h1>Assignment 07</h1>
+  <h2>Fixit's</h2>
+  <ul>
+    <li><a href="fixit1.php">Fixit #1</a></li>
+    <li><a href="fixit2.php">Fixit #2</a></li>
+    <li><a href="fixit3.php">Fixit #3</a></li>
+    <li><a href="fixit4.php">Fixit #4</a></li>
+    <li><a href="fixit5.php">Fixit #5</a></li>
+  </ul>
+  
+  <h2>Modify's</h2>
+  <ul>
+    <li><a href="modify1.html">Modify #1</a></li>
+    <li><a href="modify2.html">Modify #2</a></li>
+    <li><a href="modify3.html">Modify #3</a></li>
+  </ul>
+  
+  <h2>Code Exercises</h2>
+  <ul>
+    <li><a href="paint-estimate.html">Paint Estimate #1</a></li>
+    <li><a href="software-order.html">Software Order #2</a></li>
+    <li><a href="character.php">Character #3</a></li>
+    <li><a href="cube-test.php">Cube #4</a></li>
+    <li><a href="number-five.php">Number Five #5</a></li>
+  </ul>
+  <a href="../index.php">Home Page</a>
+</body>
 
-
-
-?>
-</body>  
 </html>
